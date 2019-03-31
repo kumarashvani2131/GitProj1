71e44992727f259217d157025a3f46d33c0464ac
DELL <DELL@192.168.0.35>
2019-03-31 16:12:05 +0530

    First Commit

---
+++ JdbcProje1/src/com/nit/jdbc/SelectTest.java
@@ -0,0 +1,64 @@
+package com.nit.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		Connection conn=null;
+		Statement st=null;
+		ResultSet rs=null;
+		boolean flage=false;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","abc");
+			 if(conn!=null)
+				 st=conn.createStatement();
+			 if(st!=null)
+				 rs=st.executeQuery("SELECT * FROM STUDENT");
+			 if(rs!=null) {
+				 while(rs.next()) {
+					 flage=true;
+					 System.out.println(rs.getInt(1)+" "+rs.getString(2));
+				 }
+				 if(flage==false) {
+					 System.out.println("data not found");
+				 }
+			 }
+		}catch (SQLException sql) {
+			sql.printStackTrace();
+		}
+		catch (ClassNotFoundException cnf) {
+			cnf.printStackTrace();
+		}
+		
+		finally {
+			try {
+				if(rs!=null)
+					rs.close();
+			}catch (SQLException sql) {
+				sql.printStackTrace();
+			}
+			
+			try {
+				if(st!=null)
+					st.close();
+			}catch (SQLException sql) {
+				sql.printStackTrace();
+			}
+			
+			try {
+				if(conn!=null)
+					conn.close();
+			}catch (SQLException sql) {
+				sql.printStackTrace();
+			}
+		}
+	}
+
+}
